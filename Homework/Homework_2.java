741c65afb80b7872f6644228417e667e4415250a
Oxana <crazyglass@list.ru>
2023-06-05 18:33:31 +0300

    Лекция 3

---
+++ Homework/Homework_2.java
@@ -0,0 +1,16 @@
+package Homework;
+
+public class Homework_2 {
+
+    Node head;
+    Node tail;
+    
+    public class Node {
+        int value;
+        Node next;
+        Node previous;
+    }
+
+    
+    
+}
